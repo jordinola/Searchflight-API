2404f836bf2a6d97c8afc1dd2076d17997038008
jjob3011 <jjob3011@gmail.com>
2020-03-05 10:12:46 -0500

    Add project files.

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Searchflight.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        public SearchController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
